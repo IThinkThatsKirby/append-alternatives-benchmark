d4513d4595b2f9df861db55b5978e7e7543d8a89
kirby ellingson <hkellingson@gmail.com>
2022-12-06 13:22:27 -0600

    big tests with new size function

--- main_test.go
+++ main_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// testbenching different ways to digitize a number.
+
 // easy readable append
 func digitize(x int) []int {
 	digit := 0
@@ -31,9 +33,10 @@ func appendDigitze(x int) []int {
 
 // make copy
 func copyDigitize(x int) []int {
+	size := howManyDigits(x)
 	digit := 0
-	digits := make([]int, 10)
-	for i := 10; x >= 1; i-- {
+	digits := make([]int, size)
+	for i := size; x >= 1; i-- {
 		digit = x % 10
 		x /= 10
 		copy(digits[i-1:], []int{digit})
@@ -41,7 +44,55 @@ func copyDigitize(x int) []int {
 	return digits
 }
 
-// testing
+// range fill
+func rangeDigitize(x int) []int {
+	size := howManyDigits(x)
+	digit := 0
+	digits := make([]int, size)
+	for i := range digits {
+		if x < 1 {
+			break
+		}
+		digit = x % 10
+		x /= 10
+		digits[len(digits)-i-1] = digit
+	}
+	return digits
+}
+
+// gets the number of digits in a number.
+func howManyDigits(x int) int {
+	digits := 0
+	for x >= 1 {
+		x /= 10
+		digits++
+	}
+	return digits
+}
+
+// range withmanuall set size
+func manualSetSizeRangeDigitize(x int) []int {
+	digit := 0
+	digits := make([]int, 10)
+	for i := range digits {
+		if x < 1 {
+			break
+		}
+		digit = x % 10
+		x /= 10
+		digits[10-i-1] = digit
+	}
+	return digits
+}
+
+func Test_howManyDigits(t *testing.T) {
+	if got := howManyDigits(1234567890); got != 10 {
+		t.Errorf("howManyDigits() = %v, want %v", got, 10)
+	} else {
+		t.Logf("howManyDigits() = %v, want %v", got, 10)
+	}
+}
+
 func Test_DigitizeMeCaptain(t *testing.T) {
 	type args struct {
 		x int
@@ -51,6 +102,14 @@ func Test_DigitizeMeCaptain(t *testing.T) {
 		want []int
 	}{
 		{args{9078562341}, []int{9, 0, 7, 8, 5, 6, 2, 3, 4, 1}},
+		{args{9078562344}, []int{9, 0, 7, 8, 5, 6, 2, 3, 4, 4}},
+		{args{9078562345}, []int{9, 0, 7, 8, 5, 6, 2, 3, 4, 5}},
+		{args{9078562346}, []int{9, 0, 7, 8, 5, 6, 2, 3, 4, 6}},
+		{args{1456489840321549}, []int{1, 4, 5, 6, 4, 8, 9, 8, 4, 0, 3, 2, 1, 5, 4, 9}},
+		{args{1234567890}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}},
+		{args{1234567891}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 1}},
+		{args{1234567892}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 2}},
+		{args{9223372036854775807}, []int{9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7}},
 	}
 	for _, tt := range tests {
 		t.Run("digitizing", func(t *testing.T) {
@@ -63,11 +122,24 @@ func Test_DigitizeMeCaptain(t *testing.T) {
 			if got := copyDigitize(tt.args.x); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("copyDigitze() = %v, want %v", got, tt.want)
 			}
+			if got := rangeDigitize(tt.args.x); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("digitze() = %v, want %v", got, tt.want)
+			}
 		})
+	}
+}
+func Test_manualSetSizeRangeDigitize(t *testing.T) {
+	if got := manualSetSizeRangeDigitize(1234567890); !reflect.DeepEqual(got, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) {
+		t.Errorf("manualSetSizeRangeDigitize() = %v, want %v", got, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0})
 	}
 }
 
 // the benchmarks
+func Benchmark_howManyDigits(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		howManyDigits(9078562341)
+	}
+}
 func Benchmark_digitize(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		digitize(9078562341)
@@ -78,8 +150,19 @@ func Benchmark_appendDigitize(b *testing.B) {
 		appendDigitze(9078562341)
 	}
 }
-func Benchmark_copyDigitize(b *testing.B) { // winner winner tofu dinner.
+func Benchmark_copyDigitize(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		copyDigitize(9078562341)
+	}
+}
+func Benchmark_rangeDigitize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rangeDigitize(9078562341)
+	}
+}
+
+func Benchmark_manualSetSizeRangeDigitize(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		manualSetSizeRangeDigitize(9078562341)
 	}
 }
