08bb2610d08f8493ea79eb1c3c3407a95d37fce0
kirby ellingson <hkellingson@gmail.com>
2022-12-06 09:54:36 -0600

    created the functions to bench

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
-package main
+package digitize_test
 
-func main() {
+// easy readable append
+func digitize(x int) []int {
+	digit := 0
+	digits := []int{}
+	for i := 0; x >= 1; i++ {
+		digit = x % 10
+		x /= 10
+		digits = append([]int{digit}, digits...)
+	}
+	return digits
+}
 
+// make append
+func appendDigitze(x int) []int {
+	digit := 0
+	digits := make([]int, 0)
+	for x >= 1 {
+		digit = x % 10
+		x /= 10
+		digits = append([]int{digit}, digits...)
+	}
+	return digits
+}
+
+// make copy
+func copyDigitize(x int) []int {
+	digit := 0
+	digits := make([]int, 10)
+	for i := 10; x >= 1; i-- {
+		digit = x % 10
+		x /= 10
+		copy(digits[i-1:], []int{digit})
+	}
+	return digits
 }
